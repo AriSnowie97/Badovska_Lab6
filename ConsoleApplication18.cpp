6a05548d20d13b81c4fb7f52d5672fba09b02b46
Ari Badovska <160342977+AriSnowie97@users.noreply.github.com>
2024-11-06 08:19:39 +0200

    Add files via upload

---
+++ ConsoleApplication18.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    double a = 10.0, b = 8.0, c = 15.0;
+    double* num1 = &a;
+    double* num2 = &b;
+    double* num3 = &c;
+    double seredne = (*num1 + *num2 + *num3) / 3;
+    cout << "Middle number: " << seredne << endl;
+    return 0;
+}
